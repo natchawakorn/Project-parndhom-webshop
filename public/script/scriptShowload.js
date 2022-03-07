97360439051c934bda68580a681628fba5231e1a
unknown <natchawakornkong11@gmail.com>
2022-03-07 22:53:09 +0700

    load out

--- public/script/scriptShowload.js
+++ public/script/scriptShowload.js
@@ -1,5 +1,17 @@
 const loadshow = document.querySelector('.loadshow');
+const link_select= document.getElementById('link_select');
+const link_home = document.getElementById('link_home');
 
-window.addEventListener('load',function(){
+function f_loadshowIn(){
     loadshow.style.display = 'none' ;
-})
+}
+window.addEventListener('load',f_loadshowIn);
+
+function f_loadshowOut(){
+    loadshow.style.display = 'block';
+}
+
+link_select.addEventListener('click',f_loadshowOut);
+link_home.addEventListener('click',f_loadshowOut);
+
+
